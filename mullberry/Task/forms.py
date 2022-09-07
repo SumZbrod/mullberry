035f5b99958ca454c4a5072bb2e3ff16b63e3c7c
sumzbrod <sumzbrod@gmail.com>
2022-09-07 15:46:30 +0300

    replace form fields

--- mullberry/Task/forms.py
+++ mullberry/Task/forms.py
@@ -2,9 +2,9 @@ from django import forms
 from .models import *
 
 class AddTask(forms.Form):
-    new_name = forms.CharField(max_length=64, label='Новое название товара')
     # nm_Id = forms.CharField(max_length=9, label='Id товара')
     nm_Id = forms.IntegerField(max_value=10**10, label='Артикул товара')
+    new_name = forms.CharField(max_length=64, label='Новое название товара')
 
     def __str__(self):
         # return f" {self.user_name=} {self.item_name=} {self.nmId=} "
